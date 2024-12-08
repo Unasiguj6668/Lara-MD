5240c54eb67efe5bc66d094f221f8c094fcd8a0d
Unasiguj6668 <unitedstairs12467@gmail.com>
2024-12-08 15:12:27 +0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "66IVhY5C#L_S9SSo8fHeOdCFQmV4ShArbsxWGyfOHbeHxbeQ_YBU", //paste your session_id
+SESSION_ID: process.env.SESSION_ID || "H6AjUYbL#A-D4kFwRdIIuYBdfXbmM477YMHgo-vk0bMgfVTIZPiw", //paste your session_id
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", 
 MODE: process.env.MODE || "public",
 AUTO_VOICE: process.env.AUTO_VOICE || "true",
